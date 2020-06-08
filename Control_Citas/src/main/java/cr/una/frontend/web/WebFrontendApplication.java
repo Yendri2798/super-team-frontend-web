e7d32335e5245343978ef2072d41768579791393
YENDRI <Yendri2798>
2020-06-07 17:15:41 -0700

    Cambios

--- Control_Citas/src/main/java/cr/una/frontend/web/WebFrontendApplication.java
+++ Control_Citas/src/main/java/cr/una/frontend/web/WebFrontendApplication.java
@@ -1,4 +1,6 @@
 package cr.una.frontend.web;
 
+
 public class WebFrontendApplication {
+    //public static void main(String[] args) { SpringApplication.run(WebFrontendApplication.class, args);}
 }
